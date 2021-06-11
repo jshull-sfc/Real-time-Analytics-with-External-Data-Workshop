4b5e5bb0ca4ccd0bc2527b1d09902fd0d0f1891a
Jason Shull <55121474+jshull-sfc@users.noreply.github.com>
2021-06-11 15:48:50 -0500

    Add files via upload

--- real-time_analytics_with_external_data.sql
+++ real-time_analytics_with_external_data.sql
@@ -1,32 +1,54 @@
---Run ahead of time
-USE ROLE SYSADMIN;
-CREATE OR REPLACE DATABASE FROSTY_DB;
+--Set up
+USE ROLE ACCOUNTADMIN; --Highest level access for this URL
+DROP DATABASE IF EXISTS DEMO_WEATHERSOURCE; --Clean up
+DROP SHARE IF EXISTS "WEATHER_2016";        --Clean up
+DROP MANAGED ACCOUNT IF EXISTS "OLAF";      --Clean up
+USE ROLE SYSADMIN; --Change to lower privileged DBA role
+CREATE OR REPLACE DATABASE FROSTY_DB;  --Create Database
 
-CREATE OR REPLACE WAREHOUSE WINTER_WH WITH WAREHOUSE_SIZE = 'XXLARGE' WAREHOUSE_TYPE = 'STANDARD' 
-AUTO_SUSPEND = 600 AUTO_RESUME = TRUE MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 2 SCALING_POLICY = 'STANDARD';
+CREATE OR REPLACE WAREHOUSE WINTER_WH WITH WAREHOUSE_SIZE = 'MEDIUM' WAREHOUSE_TYPE = 'STANDARD' 
+AUTO_SUSPEND = 600 AUTO_RESUME = TRUE MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 2 SCALING_POLICY = 'STANDARD'; --Create virtual compute warehouse
 
-create or replace table DAILY_14_TOTAL as
-select *
-from snowflake_sample_data.weather.DAILY_14_TOTAL;
+/*
+1. Go out to the Data Marketplace.
+2. Search for Weather Source.
+3. Select Weather Source - Global Weather & Climate Data for BI.
+4. Get the data and name the Database DEMO_WEATHERSOURCE.
+5. Leave the default permissions for SYSADMIN.
 
-create or replace table WEATHER_14_TOTAL as
-select *
-from snowflake_sample_data.weather.WEATHER_14_TOTAL;
+How many Deliveries will be delayed due to snowfall?
 
---Reset
-USE ROLE ACCOUNTADMIN;
-DROP SHARE "WEATHER_2019";
-DROP MANAGED ACCOUNT "OLAF";
+When it snows in excess of 6 inches per day, my company experiences delivery delays.
+How many deliveries may have been delayed?
+*/
 
---Start here
-create or replace secure view DAILY_14_TOTAL_2019_VW as
-select *
-from DAILY_14_TOTAL
-where t >= to_date('01/01/2019','MM/DD/YYYY');
+SELECT   COUNTRY,
+         POSTAL_CODE,
+         DATE_VALID_STD,
+         TOT_SNOWFALL_IN 
+FROM     DEMO_WEATHERSOURCE.STANDARD_TILE.HISTORY_DAY
+WHERE    COUNTRY='US'
+AND      TOT_SNOWFALL_IN > 6.0 
+ORDER BY POSTAL_CODE, DATE_VALID_STD
+LIMIT 10;
 
-create or replace secure view WEATHER_14_TOTAL_2019_VW as
-select *
-from WEATHER_14_TOTAL
-where t >= to_date('01/01/2019','MM/DD/YYYY');
+-- Create a table for snowfall greater than 6 inches.
+CREATE OR REPLACE TABLE FROSTY_DB.PUBLIC.HISTORY_DAY as
+SELECT   COUNTRY,
+         POSTAL_CODE,
+         DATE_VALID_STD,
+         TOT_SNOWFALL_IN 
+FROM     DEMO_WEATHERSOURCE.STANDARD_TILE.HISTORY_DAY
+WHERE    COUNTRY='US'
+AND      TOT_SNOWFALL_IN > 6.0 
+ORDER BY POSTAL_CODE, DATE_VALID_STD;
 
+--Follow documentation to create reader account in the Web UI.
 
+--Start here unless you do not have time to wait for the new URL to be active. Then you could complete everything ahead of time.
+use database frosty_db;
+use warehouse WINTER_WH;
+
+CREATE OR REPLACE SECURE VIEW FROSTY_DB.PUBLIC.HISTORY_DAY_SVW as
+SELECT *
+FROM FROSTY_DB.PUBLIC.HISTORY_DAY;
